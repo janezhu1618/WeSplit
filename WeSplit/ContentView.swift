19f2b9c94a8db2dda96b01e524281821350384b9
janezhu1618 <janezhu@pursuit.org>
2020-02-25 20:11:22 -0500

    create file

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jane Zhu on 2/25/20.
+//  Copyright © 2020 JaneZhu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
